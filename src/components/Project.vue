782fbf7502e44dde0defd0bbb7057e027401617d
DevDhaif <devdhaif@gmail.com>
2022-05-01 23:50:16 +0200

    done project section overlap

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,16 +1,48 @@
 <template>
-    <article class="relative p-8 space-y-6 bg-[url('{{data.imgSrc}}')]  bg-blend-overlay">
-        <h3 class="text-2xl">{{ data.title }}</h3>
-        <h3 class="text-2xl">{{ data.imgSrc }}</h3>
-        <p>{{ data.desc }}</p>
-        <a href="#" target="_blank">Live</a>
-        <a v-bind:href="data.demoLink" target="_blank">Demo</a>
-        <h4 class="text-xl">Technologies used :</h4>
-        <ul class="flex py-4 gap-x-4">
-            <li v-for="(item, index) in data.techs">
-                {{ item }}
-            </li>
-        </ul>
+    <article
+        class="gap-8 px-8 py-4 my-8 space-y-10 rounded-md shadow-lg md:space-y-0 md:grid-cols-8 place-items-stretch md:grid shadow-cyan-300/30">
+
+        <div class="order-1 col-start-5 col-end-11 row-start-1 space-y-8 md:text-right">
+            <h3 class="mx-auto text-2xl text-cyan-300">{{ data.title }}</h3>
+            <p class="p-4 text-lg leading-7 tracking-wide bg-gray-900 rounded-md ">{{ data.desc }}</p>
+
+            <h4 class="w-full mx-auto text-xl">Used <span class="text-cyan-300">tech</span>nologies :</h4>
+            <ul class="flex w-full mx-auto md:justify-end gap-x-4">
+                <li class="underline underline-offset-2 decoration-cyan-300" v-for="(item, index) in data.techs">
+                    {{ item }}
+                </li>
+            </ul>
+        </div>
+        <div
+            class="max-w-2xl col-start-1 col-end-6 row-start-1 px-4 pt-4 mx-auto border-t border-l md:pt-0 rounded-3xl border-cyan-300">
+            <img class="object-contain object-center w-full py-4 " v-bind:src="data.imgSrc" alt="project image">
+            <div class="flex items-center justify-around max-w-xl py-2 mx-auto ">
+                <a class="block cursor-pointer text-cyan-500 hover:text-cyan-400" href="#" target="_blank">
+                    <span class="">
+                        <svg class="w-8 h-8 transition-transform duration-75 transform cursor-pointer hover:scale-105 "
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor"
+                            stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+                        </svg>
+                    </span>
+                    <span class="text-center"> Code </span>
+                </a>
+                <a class="block cursor-pointer text-cyan-500 hover:text-cyan-400" v-bind:href="data.demoLink"
+                    target="_blank">
+                    <span aria-hidden="true">
+
+                        <svg class="w-8 h-8 transition-transform duration-75 transform cursor-pointer hover:scale-105 "
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor"
+                            stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                        </svg>
+                    </span>
+                    <span> Live </span>
+                </a>
+            </div>
+        </div>
 
     </article>
 </template>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,16 +1,17 @@
 <template>
-    <section id="projects" class="w-full py-4 my-8 text-white ">
+    <section id="projects" class="w-full p-4 my-8 text-white ">
         <div class="max-w-6xl mx-auto ">
             <h2 class="text-3xl text-center md:text-left "> Projects I worked on </h2>
-            <h3 class="text-2xl ">Latest Projects</h3>
             <Project :data="{
                 title: 'Anime News',
-                desc: ' simple anime ',
+                desc: 'Simple anime app to explore, search and filter animes,and get the latest anime news using jikan api.',
                 imgSrc: 'https://user-images.githubusercontent.com/77447520/159186205-fa01583e-58f2-4490-aa51-5478e5aa6aad.png',
                 githubLink: 'https://github.com/DevDhaif/my-anime',
                 demoLink: 'https://my-anime-seven.vercel.app/',
                 techs: ['HTML', 'React Js', 'Tailwind css', 'Axios']
             }" />
+
+
         </div>
     </section>
 </template>
